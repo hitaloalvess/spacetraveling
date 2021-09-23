4064c05bf69f004850ca418beb2beb1902fb5a06
hitaloalvess <hitalo.ralves@outlook.com>
2021-09-23 16:21:14 -0300

    Implementando comportamento fallback true do getStaticProps e calculo estimativo de tempo de leitura do post

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,14 +1,18 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
+import Head from 'next/head';
 
 import format from 'date-fns/format';
 import { ptBR } from 'date-fns/locale';
 
 import { RichText } from 'prismic-dom';
 import { getPrismicClient } from '../../services/prismic';
+import Prismic from '@prismicio/client';
 
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
 import { FaCalendar, FaUser, FaClock } from 'react-icons/fa';
+import { useRouter } from 'next/router';
+
 
 interface Post {
   first_publication_date: string | null;
@@ -27,14 +31,31 @@ interface Post {
   };
 }
 
-interface PostProps {
-  post: Post;
+interface ContentProps{
+  heading: string;
+    body: {
+      text: string;
+    }[];
 }
 
-export default function Post( { post } : PostProps) {
+interface PostProps {
+  post: Post;
+  readingTimePost: number;
+}
 
-  return(
+export default function Post( { post, readingTimePost } : PostProps) {
+  const router = useRouter()
+
+  if(router.isFallback){
+      return <h1>Carregando...</h1>
+  }
+  
+    return(
       <>
+        <Head>
+          <title>Post | {post.data.title}</title>
+        </Head>
+
         <img className={styles.logo} src={post.data.banner.url} alt="logo" />
         <main className={`${styles.postContainer} ${commonStyles.container}`}>
           <section className={commonStyles.content}>
@@ -52,7 +73,7 @@ export default function Post( { post } : PostProps) {
 
               <div >
                 <FaClock />
-                  4min
+                {readingTimePost}min aprox.
               </div>
             </div>
 
@@ -73,25 +94,58 @@ export default function Post( { post } : PostProps) {
         </main>
       </>
   );
+  
 }
 
 export const getStaticPaths : GetStaticPaths = async () => {
 
+  const prismic = getPrismicClient();
+  const response = await prismic.query(
+    [ Prismic.predicates.at('document.type', 'posts')],
+    {
+      fetch:[],
+      pageSize: 3
+    }
+  );
+  
+  const params = response.results.reduce( (acc, item) => {
+    acc.push({params: {slug: item.uid}}) 
+    return acc;
+  }, [])
+
   return{
-    paths:[],
-    fallback: 'blocking',
+    paths:[
+      // ...params
+      {params: {slug:'novas-features-javascript'}},
+    ],
+    fallback: true,
   }
 };
 
 export const getStaticProps: GetStaticProps = async ({params}) => {
 
   const { slug } = params
-  console.log(slug)
+  
   const prismic = getPrismicClient();
   const response = await prismic.getByUID('posts', String(slug), {});
 
+
+  function calculateReadingTimePost(){
+      const wordPerMinute = 200;
+
+      const numberWords = response.data.content.reduce( (acc : String[], item: ContentProps) => {
+        const wordsHeading = item.heading.split(' ');
+        const wordsBody = RichText.asText([...item.body]).split(' ');
+          acc.push(...wordsHeading, ...wordsBody)
+          return acc
+      }, [])
+
+     const readingTime = Math.ceil( numberWords.length/ wordPerMinute);
+
+     return readingTime;
+  }
+  
   const content = response.data.content.map( item => {
-   
     return {
       heading: item.heading,
       body: RichText.asHtml([...item.body])
@@ -118,7 +172,8 @@ export const getStaticProps: GetStaticProps = async ({params}) => {
 
   return{
     props:{
-      post
+      post,
+      readingTimePost : calculateReadingTimePost()
     }
   }
 };
